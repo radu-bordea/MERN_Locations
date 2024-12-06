345a3b7f18171808bc30c919411322bb65cb8da0
radu-bordea <radu.bordea.dev@gmail.com>
2024-12-06 17:09:37 +0200

    create useritem, userlist components and css sheets

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './UserItem.css'
+
+const UserItem = () => {
+  return (
+    <div>UserItem</div>
+  )
+}
+
+export default UserItem
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import UserList from "../components/UserList";
 
 const Users = () => {
-  return (
-    <h2>Users works!</h2>
-  )
-}
+  const USERS = [
+    {
+      id: "ul",
+      name: "Bordea Radu",
+      image:
+        "https://travelbird-images.imgix.net/5b/cf/5bcfc302a6063ac933fc5a1d24d93615?auto=compress%2Cformat&crop=faces%2Cedges%2Ccenter&dpr=2&fit=crop&h=700&upscale=true&w=1050",
+      places: 3,
+    },
+  ];
 
-export default Users
+  return <UserList items={USERS} />;
+};
+
+export default Users;
